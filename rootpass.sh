8ab660bb95bd8c78a9682fe8ea9d0f345d45cf83
Bankzza555666 <33499449+Bankzza555666@users.noreply.github.com>
2021-09-15 04:52:53 +0700

    Update rootpass.sh

--- rootpass.sh
+++ rootpass.sh
@@ -3,9 +3,9 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
+
 clear
-sudo passwd
 
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
-sudo service ssh restart
+/etc/init.d/ssh restart
