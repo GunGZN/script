5addcdddb79667a71042a457ab448fa1dfb114cc
Bankzza555666 <33499449+Bankzza555666@users.noreply.github.com>
2021-09-15 05:00:48 +0700

    Update rootpass.sh

--- rootpass.sh
+++ rootpass.sh
@@ -6,6 +6,18 @@ MYIP=$(wget -qO- icanhazip.com);
 
 clear
 
+echo -e ""
+echo -e "**************************************"
+echo -e "             สคริปโดย sp vpn-th"
+echo -e ""
+echo -e "     [1]  ตั้งรหัสสำหรับ ROOT"
+echo -e "     [2]  รีบูต SSH (ต้องเลือก)"
+echo -e "     [x]  ออก"
+echo -e "**************************************"
+echo -e ""
+read -p "    กรุณาเลือก [1,2 หรือ x] :  "
+
+
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 /etc/init.d/ssh restart
