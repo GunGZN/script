3799d48159f1164d70c6a23dcd635f5dcbfca631
Bankzza555666 <33499449+Bankzza555666@users.noreply.github.com>
2021-09-15 20:48:27 +0700

    Update changeport.sh

--- changeport.sh
+++ changeport.sh
@@ -6,11 +6,12 @@ MYIP=$(wget -qO- icanhazip.com);
 clear
 echo -e ""
 echo -e "**************************************"
-echo -e "             สคริปโดย sp vpn-th"
+echo -e "             กรุณาทำตามขั้นตอน 1,2"
 echo -e ""
 echo -e "     [1]  เปลี่ยนพ็อตหลักเป็น 80"
 echo -e "     [2]  รีบูต VPS (ต้องเลือก)"
 echo -e "     [x]  ออก"
+echo -e "*หากต้องการใช้ VMESSต่างๆต้องไปเปลี่ยนพ็อตVMESSก่อน"
 echo -e "**************************************"
 echo -e ""
 read -p "    กรุณาเลือก [1,2 หรือ x] :  " changeport
@@ -20,7 +21,7 @@ sleep 1
 case $changeport in   
                 
                 2)
-                sed -i 's/#listen       81 /listen       80/' /etc/nginx/conf.d/vps.conf
+                sed -i 's/listen       81 /listen       80/' /etc/nginx/conf.d/vps.conf
                 echo -e ""
                 echo -e "**************************************"
                 echo -e ""
