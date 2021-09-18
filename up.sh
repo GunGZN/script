821092ac065e706f6b9fec7f687f991c2a4a4978
SPVPN-TH <33499449+Bankzza555666@users.noreply.github.com>
2021-09-19 00:43:05 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -11,8 +11,10 @@ wget -O admin "https://raw.githubusercontent.com/Bankzza555666/script/main/admin
 wget -O menu "https://raw.githubusercontent.com/Bankzza555666/script/main/menu.sh"
 wget -O m "https://raw.githubusercontent.com/Bankzza555666/script/main/menu.sh"
 wget -O backup "https://raw.githubusercontent.com/Bankzza555666/script/main/backup.sh"
+wget -O up "https://raw.githubusercontent.com/Bankzza555666/script/main/up.sh"
 chmod +x backup
 chmod +x m
 chmod +x menu
 chmod +x admin
+chmod +x up
 exit
